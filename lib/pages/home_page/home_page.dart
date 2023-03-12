4a3d8d64e5f76c966d41097026f91c3880cbb383
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-03-12 13:05:19 +0530

    Change title to "Viyukta"

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome to Viyukta'),
+        title: const Text('Viyukta'),
       ),
       body: SafeArea(
         child: Container(
