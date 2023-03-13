f57a136c974837770a9e6e730d8346f9f435ed57
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-03-13 21:51:53 +0530

    Add CSV upload page and read the CSV file

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -3,7 +3,8 @@ import 'package:page_transition/page_transition.dart';
 import 'package:viyukta/pages/commitment_form/commitment_form.dart';
 import 'package:viyukta/pages/commitment_list/commitment_list.dart';
 import 'package:viyukta/pages/home_page/widgets/organization_details_card.dart';
-import 'package:viyukta/pages/upload_commitment/upload_commitment.dart';
+
+import '../upload_commitment/upload_commitment.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
--- lib/pages/upload_commitment/upload_commitment.dart
+++ lib/pages/upload_commitment/upload_commitment.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:csv/csv.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:csv/csv.dart';
+import 'package:viyukta/theme/theme.dart';
 
-import '../../provider/upload_file.dart';
+import '../../provider/c_s_v_data.dart';
 
 class UploadCommitment extends ConsumerWidget {
   UploadCommitment({super.key});
@@ -15,81 +16,77 @@ class UploadCommitment extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final data = ref.watch(fileProvider).data;
+    final data = ref.watch(csvProvider).csvData;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Upload Commitment'),
-        automaticallyImplyLeading: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                onPressed: () {
-                  _pickFile(ref);
-                },
-                child: const Text('Upload Excel'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    _pickFile(ref);
+                  },
+                  child: const Text('Upload CSV'),
+                ),
               ),
-            ),
-            ListView.builder(
-              itemCount: data.length,
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemBuilder: (_, index) {
-                return Card(
-                  margin: const EdgeInsets.all(3),
-                  color: index == 0 ? Colors.amber : Colors.white,
-                  child: ListTile(
-                    leading: Text(
-                      data[index][0].toString(),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: index == 0 ? 18 : 15,
-                          fontWeight:
-                              index == 0 ? FontWeight.bold : FontWeight.normal,
-                          color: index == 0 ? Colors.red : Colors.black),
+              const SizedBox(
+                height: 32.0,
+              ),
+              filePath != null
+                  ? Column(
+                      children: [
+                        const Text('CSV file loaded successfully!'),
+                        const SizedBox(height: 32.0),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'CSV Data',
+                            style: textTheme().labelMedium,
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        Text('$data'),
+                        const SizedBox(height: 40.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            const SizedBox(width: 32.0),
+                            ElevatedButton(
+                                onPressed: () {},
+                                child: const Text('Submit & Save')),
+                          ],
+                        )
+                      ],
+                    )
+                  : const Center(
+                      child: Text('No Data'),
                     ),
-                    title: Text(
-                      data[index][3],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: index == 0 ? 18 : 15,
-                          fontWeight:
-                              index == 0 ? FontWeight.bold : FontWeight.normal,
-                          color: index == 0 ? Colors.red : Colors.black),
-                    ),
-                    trailing: Text(
-                      data[index][4].toString(),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: index == 0 ? 18 : 15,
-                          fontWeight:
-                              index == 0 ? FontWeight.bold : FontWeight.normal,
-                          color: index == 0 ? Colors.red : Colors.black),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void _pickFile(ref) async {
+  void _pickFile(WidgetRef ref) async {
     final result = await FilePicker.platform.pickFiles(allowMultiple: false);
 
     // if no file is picked
-    if (result == null) {
-      return; // we will log the name, size and path of the Y first picked file (if multiple are selected)
-    }
-
-    print(result.files.first.name);
-
+    if (result == null) return;
+    // we will log the name, size and path of the
+    // first picked file (if multiple are selected)
     filePath = result.files.first.path!;
 
     final input = File(filePath!).openRead();
@@ -98,8 +95,6 @@ class UploadCommitment extends ConsumerWidget {
         .transform(const CsvToListConverter())
         .toList();
 
-    print(fields);
-
-    ref.watch(fileProvider.notifier);
+    ref.watch(csvProvider.notifier).getFile(fields);
   }
 }
