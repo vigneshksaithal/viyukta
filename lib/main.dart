68009dd7f78117c5d5ea0cab847e69625512aa6c
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-03-14 11:13:40 +0530

    Fix imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:viyukta/pages/home_page/home_page.dart';
+import 'pages/home_page/home_page.dart';
 
 import 'theme/theme.dart';
 
