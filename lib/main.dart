7aa1d9ba6a05407b41f141f826abcbf0d6e10e45
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2023-03-17 15:07:18 +0530

    Remove unused packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'pages/home/home.dart';
 
 import 'theme/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Hive.initFlutter();
 
   runApp(
     const ProviderScope(
